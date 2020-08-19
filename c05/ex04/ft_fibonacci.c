82609c01883daa3161eacc612a031be6b470ceb7
Seongjun <sehwang@c5r1s5.42seoul.kr>
2020-08-19 22:36:04 +0900

    right before summit

--- c05/ex04/ft_fibonacci.c
+++ c05/ex04/ft_fibonacci.c
@@ -6,7 +6,7 @@
 /*   By: seongjun <seongjun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 20:28:03 by seougjun          #+#    #+#             */
-/*   Updated: 2020/08/19 17:18:47 by seongjun         ###   ########.fr       */
+/*   Updated: 2020/08/19 22:34:58 by sehwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,4 @@ int		ft_fibonacci(int index)
 		return (1);
 	else
 		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
-
 }
