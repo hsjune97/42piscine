2812f398ad8fa1de249dbaf3741c56e462d0ebfa
Seongjun Hwang <hsjune97@gmail.com>
2020-08-20 10:26:28 +0900

    0820_1026

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -1,8 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongjun <seongjun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/20 08:27:58 by seongjun          #+#    #+#             */
+/*   Updated: 2020/08/20 08:28:00 by seongjun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-char	*ft_strup(char *src)
+int		ft_strlen(char *str);
+
+char	*ft_strdup(char *src)
 {
-	return (0);
+	char *dest;
+	int i;
+
+	i = 0;
+	dest = (char*)malloc(sizeof(char) * ft_strlen(src) + 1);
+	if (dest == NULL)
+		return (0);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
-//not done
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -1,8 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongjun <seongjun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/20 08:49:06 by seongjun          #+#    #+#             */
+/*   Updated: 2020/08/20 08:49:10 by seongjun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 int		*ft_range(int min, int max)
 {
-	return (0);
-}
+	int i;
+	int *range;
 
-//not done
+	if (min >= max)
+		return (0);
+	range = (int*)malloc(sizeof(int) * (max - min));
+	if (!range)
+		return (0);
+	i = -1;
+	while (max > min + ++i)
+	{
+		range[i] = min + i;
+	}
+	return (range);
+}
