70fb76708ec2ca100b104559553c552515d1b3ce
Seongjun Hwang <hsjune97@mgmail.com>
2020-09-04 17:01:02 +0900

    0904_1700

--- final/Set7/flood_fill/flood_fill.c
+++ final/Set7/flood_fill/flood_fill.c
@@ -41,7 +41,7 @@ int main(void)
     //char **area;
     t_point size = { 8, 5 };
     t_point begin = { 2, 2 };
-    char *zone[] = {
+    char **zone = {
         "11111111",
         "10001001",
         "10010001",
